4e393f0ed0e21ee00037016e46f9cf17b1c20b2a
Edwin Melara <melara0606@gmail.com>
2019-02-02 22:53:03 -0600

    Condiciones y bucle

---
+++ condiciones-bucle.py
@@ -0,0 +1,20 @@
+l1 = [1,2,5]
+l2 = [7,8,9]
+l3 = [12,89,58]
+
+# for x, y, z in zip(l1, l2, l3):
+#   print(x, y, z)
+
+lista_cadenas = ["Aprendiendo", "Python", "con", "DevCodela"]
+
+lista_longitudes = [len(i) for i in lista_cadenas]
+# print(lista_cadenas)
+# print(lista_longitudes)
+
+a = 10
+b = 20
+
+variable1 = "Mayor" if (a > b) else "Menor"
+variable2 = ["Menor", "Mayor"] [b > a]
+
+# print(variable1, variable2)
