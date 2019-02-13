3fc8d068f0c034889c41954d000e02a92035a107
Edwin Melara <melara0606@gmail.com>
2019-02-12 22:17:02 -0600

    Iniciando con thereading

---
+++ thread/thread.py
@@ -0,0 +1,35 @@
+import threading
+import time
+
+global cantidad
+cantidad = 100
+
+semaforo = threading.Semaphore(1)
+
+class MyThread(threading.Thread):
+  def __init__(self, usuario, monto):
+    threading.Thread.__init__(self)
+    self.usuario = usuario
+    self.monto = monto
+
+  def run(self):
+    global cantidad
+    try:
+      semaforo.acquire()
+      cantidad = cantidad + {
+        'Jean': lambda x: -x,
+        'Carlos': lambda x: +x,
+        'Julio': lambda x: -x
+      }[self.usuario](self.monto)
+
+      print("%s esta realizado operaciones y su valor es %s" % (self.usuario, cantidad))
+      time.sleep(2)
+      semaforo.release()
+    except Exception as e:
+      print(e)
+
+if __name__ == '__main__':
+  hilos = [MyThread('Jean', 20), MyThread('Carlos', 40), MyThread('Julio', 60)]
+
+  for hilo in hilos:
+    hilo.start()
